5cfa8833278a6c272e17e2593c79b9c4b8f187ff
Fabian Bucher <fabian.bucher@quick-lohn.de>
2023-08-30 16:11:36 +0200

    meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,11 @@ export default defineNuxtConfig({
       source: './assets/icon.png'
     },
     manifest: {
-      theme_color: '#ffffff'
+      name: 'Niklas-Formel',
+      short_name: 'Niklas-Formel',
+      description: 'Calculate hiking time based on distance, ascent and descent.',
+      theme_color: '#008B8B',
+      background_color: '#ffffff',
     }
   },
   css: ['~/assets/main.css', '~/node_modules/@csstools/normalize.css/opinionated.css']
